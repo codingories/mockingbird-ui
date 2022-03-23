02725b13b51ac8b9ffa185c33d24f4280fb0a30a
朱骏杰 <zhujunjie@sundear.com>
2022-03-23 09:51:25 +0800

    6-3完成

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import classNames from 'classnames';
 
 // 字符串字面量替换enum，这种方式更简便
 type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
 
 export interface MenuProps {
   defaultIndex?: number;
   className?: string;
   mode?: MenuMode;
   style?: React.CSSProperties;
-  onSelect?: (selectedIndex: number) => void;
+  onSelect?: SelectCallback;
 }
 
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({index: 0});
+
 const Menu: React.FC<MenuProps> = (props) => {
-  const {className, mode, style, children, defaultIndex} = props;
+  const {className, mode, style, children, defaultIndex, onSelect} = props;
+  const [currentActive, setCurrentActive] = useState(defaultIndex);
   const classes = classNames('viking-menu', className, {
     'menu-vertical': mode === 'vertical'
   });
+  const handleClick = (index: number) => {
+    setCurrentActive(index)
+    if(onSelect) {
+      onSelect(index)
+    }
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick
+  }
+
   return (
     <ul className={classes} style={style}>
+      <MenuContext.Provider value = {passedContext}>
       {children}
+      </MenuContext.Provider>
     </ul>
   );
 };
