3503e110d70d8ec39a9617ca88a52e8e9a73c5a3
朱骏杰 <zhujunjie@sundear.com>
2022-03-25 14:07:24 +0800

    6-7增加subMenu完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,33 @@ import React from 'react';
 import Button, {ButtonSize, ButtonType} from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/subMenu';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <Menu mode="vertical" defaultIndex={0} onSelect={(index) => { alert(index) }}>
-          <MenuItem index={0}>
+        <Menu mode="vertical" defaultIndex={0} onSelect={(index) => { alert(index); }}>
+          <MenuItem>
             cool link
           </MenuItem>
-          <MenuItem index={1} disabled>
+          <MenuItem disabled>
             cool link 2
           </MenuItem>
-          <MenuItem index={2}>
+          <SubMenu title="dropdown">
+            <MenuItem>
+              dropdown 1
+            </MenuItem>
+            <MenuItem>
+              dropdown 2
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>
             cool link 3
           </MenuItem>
         </Menu>
-        <Button className="custom"> Hello </Button>
-        <Button disabled> Disabled Button </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
-      <p>
+
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
