e5f13c1f66df2f72d90f8db03dcfef061c90bc23
朱骏杰 <zhujunjie@sundear.com>
2022-03-26 14:18:45 +0800

    6-10完成

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {cleanup, fireEvent, render, RenderResult} from '@testing-library/react';
+import {cleanup, fireEvent, render, RenderResult, waitFor} from '@testing-library/react';
 
 import Menu, {MenuProps} from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu'
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -27,13 +28,33 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>
         xyz
       </MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>
+          drop1
+        </MenuItem>
+      </SubMenu>
     </Menu>
   );
 };
+const createStyleFile = () => {
+  const cssFile: string = `
+    .viking-submenu {
+      display: none;
+    }
+    .viking-submenu.menu-opened {
+      display:block;
+    }
+  `
+  const style = document.createElement('style')
+  style.innerHTML = cssFile
+  return style
+}
+
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
 describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile())
     menuElement = wrapper.getByTestId('test-menu');
     activeElement = wrapper.getByText('active');
     disabledElement = wrapper.getByText('disabled');
@@ -41,7 +62,8 @@ describe('test Menu and MenuItem component', () => {
   it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('viking-menu test');
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    // 只选择第一级的
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
@@ -50,10 +72,10 @@ describe('test Menu and MenuItem component', () => {
     fireEvent.click(thirdItem);
     expect(thirdItem).toHaveClass('is-active');
     expect(activeElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    expect(testProps.onSelect).toHaveBeenCalledWith("2");
     fireEvent.click(disabledElement);
     expect(disabledElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith("1");
   });
   it('should render vertical mode when mode is set to vertical', () => {
     cleanup();
@@ -61,6 +83,21 @@ describe('test Menu and MenuItem component', () => {
     const menuElement = wrapper.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
   });
+  it('should show dropdown items when hover on subMenu', async ()=>{
+    // 和getByText的区别，返回htmlElement或者是null，这里可能不存在
+    expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    const dropdownElement = wrapper.getByText('dropdown')
+    fireEvent.mouseEnter(dropdownElement)
 
+    await waitFor(() => {
+      expect(wrapper.queryByText('drop1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await waitFor(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible();
+    })
+  })
 });
 
