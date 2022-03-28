85aa288c836df08d7b1467910f0f2815d9811b8a
Ories <tellt@126.com>
2022-03-28 09:43:06 +0800

    7-4搞定横向菜单

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -2,6 +2,7 @@ import React, {useState, FunctionComponentElement, useContext} from 'react';
 import classNames from 'classnames';
 import {MenuContext} from './menu';
 import {MenuItemProps} from './menuItem';
+import Icon from '../Icon/icon';
 
 export interface SubMenuProps {
   index?: string;
@@ -70,6 +71,7 @@ const SubMenu: React.FC<SubMenuProps> = ({index, title, children, className}) =>
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" onClick={handleClick} {...clickEvents}>
         {title}
+        <Icon  icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
