bbec3c25ba87a9f44dc68acd26d77e900cbcbd7b
Ories <tellt@126.com>
2022-03-28 09:30:18 +0800

    完成7-3添加theme的css

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <Icon icon="arrow-down" theme="danger" size="10x"/>
+        <Icon icon="arrow-down" theme="primary" size="10x"/>
         {/*<FontAwesomeIcon icon={faCoffee} size="10x"/>*/}
         <Menu mode="vertical" defaultIndex="0" onSelect={(index) => { alert(index); }} defaultOpenSubMenus={['2']}>
           <MenuItem>
