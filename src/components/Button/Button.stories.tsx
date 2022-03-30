29573b9324945efebf2863b6e48b7d22f91d9084
Ories <tellt@126.com>
2022-03-30 15:08:33 +0800

    完成了8-4添加button的story book

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -8,5 +8,25 @@ const defaultButton = () => (
   <Button onClick={action('clicked')}> default button </Button>
 )
 
+// buttonWithSize
+const buttonWithSize = () => (
+  <>
+    <Button size="lg"> large button </Button>
+    <Button size="sm"> large button </Button>
+  </>
+)
+
+// buttonWithType
+const buttonWithType = () => (
+  <>
+    <Button btnType="primary"> large button </Button>
+    <Button btnType="danger"> danger button </Button>
+    <Button btnType="link" href="https://baidu.com"> link button </Button>
+  </>
+)
+
+
 storiesOf('Button Component', module)
-  .add('Button', defaultButton)
+  .add('默认 Button', defaultButton)
+  .add('不同尺寸的 Button', buttonWithSize)
+  .add('不同类型的 Button', buttonWithType)
