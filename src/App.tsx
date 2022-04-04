199b65997876183db28bf81509c0e13ff225e2c4
Ories <tellt@126.com>
2022-04-04 09:49:12 +0800

    10-3完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,29 @@
-import React, {useState} from 'react';
-// import Menu from './components/Menu/menu';
-// import MenuItem from './components/Menu/menuItem';
-// import SubMenu from './components/Menu/subMenu';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import Transition from './components/Transition/transition';
-import Button from './components/Button/button'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import Icon from './components/Icon/icon';
-import {Input} from './components/Input/input';
-// import {action} from '@storybook/addon-actions';
-library.add(fas)
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 
 const App: React.FC = () => {
-  const [ show, setShow ] = useState(false);
+  const [title, setTitle] = useState('');
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1', {
+      headers: {
+        'X-Requested-with': 'XMLHttpRequest',
+      },
+      responseType: 'json',
+    })
+      .then(res => {
+        console.log(res.data);
+        setTitle(res.data.title)
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <header>
-        <Icon theme="primary" icon="coffee" size="10x"/>
-
-        <Button size="lg" onClick={() => { setShow(!show) }}>
-            Toggle
-        </Button>
-        <Transition in={show} timeout={300} animation="zoom-in-top">
-          <div>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-          </div>
-        </Transition>
-        <Transition in={show} timeout={300} animation="zoom-in-top" wrapper>
-          <Button btnType="primary" size="lg">A Large Button</Button>
-        </Transition>
+        <h1>{title}</h1>
       </header>
-      <p>defaultInput</p>
-      <Input
-        style={{width: '300px'}}
-        placeholder="placeholder"
-        onChange={()=>{}}
-      />
-      <p>disabledInput</p>
-      <Input
-        style={{width: '300px'}}
-        placeholder="disabled input"
-        disabled
-      />
-      <p>iconInput</p>
-      <Input
-        style={{width: '300px'}}
-        placeholder="input with icon"
-        icon="search"
-      />
-      <p>sizeInput</p>
-      <>
-        <Input
-          style={{width: '300px'}}
-          defaultValue="large size"
-          size="lg"
-        />
-        <Input
-          style={{width: '300px'}}
-          placeholder="small size"
-          size="sm"
-        />
-      </>
-      <p>prependInput</p>
-      <>
-        <Input
-          style={{width: '300px'}}
-          defaultValue="prepend text"
-          prepend="https://"
-        />
-        <Input
-          style={{width: '300px'}}
-          defaultValue="google"
-          append=".com"
-        />
-      </>
     </div>
   );
-}
+};
 
 export default App;
