cfba637c1debcc344b6aa365debc6cd42fbc6861
Ories <tellt@126.com>
2022-04-09 12:01:48 +0800

    完成12-5

---
+++ src/components/Welcome/Welcome.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Component extends React.Component {
+  public render() {
+    return(
+      <div className="Component">
+
+      </div>
+    );
+  }
+}
+export default Component;
